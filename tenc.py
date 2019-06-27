de9981ca19eb3550e2e74b84cad552d684b17871
Charlescream <lipenglijia@gmail.com>
2019-06-28 00:07:20 +0800

    提交注释

--- tenc.py
+++ tenc.py
@@ -4,36 +4,26 @@ from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from MySQLdb import connect
 import time
+#创建一个集合
 aa=[]
 def tencent_demo():
 
-    for i in range(1,4):
+    for i in range(1,4):#爬取前3页
+        #谷歌浏览器
         driver = webdriver.Chrome()
 
         driver.get("https://careers.tencent.com/search.html?index="+str(i))
 
         soup = bs(driver.page_source,"lxml")
-
+        #职位
         positions = soup.find_all("h4",{"class":"recruit-title"})
-        # for each_p in positions:
-        #     #职位
-        #     #print(each_p)
-        #     pass
+
+        #城市和时间
         citys = soup.find_all("p",{"class":"recruit-tips"})
-        # #print(name)
-        # #print(city)
-        # for each in citys:
-        #     each_city = each.find_all('span')
-        #     #print(each.find_all('span')[1].get_text())
-        #     #工作地点
-        #     # print(each_city[1].get_text())
-        #     #发布时间
-        #     # print(each_city[3].get_text())
+
+        #具体细节
         details = soup.find_all("p",{"class":"recruit-text"})
-        # for e in details:
-        #     #具体详情
-        #     #print(e.get_text())
-        #     aa.append(e.get_text())
+
         for position,city,publish_time,detail in zip(positions,citys,citys,details):
             # print("职位："+position.get_text().strip()+"\n"+
             #       "工作地点："+city.find_all('span')[1].get_text().strip()+"\n"+
